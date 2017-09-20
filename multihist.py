de5229a9fdc7e9e73b001a4b2264d4956aa9cc95
Jelle Aalbers <jelleaalbers@gmail.com>
2017-09-20 13:15:24 +0200

    Bump version to 0.5.4

--- multihist.py
+++ multihist.py
@@ -47,7 +47,7 @@ COLUMNAR_DATA_SOURCES = tuple(COLUMNAR_DATA_SOURCES)
 
 from operator import itemgetter
 
-__version__ = '0.5.3'
+__version__ = '0.5.4'
 
 
 class CoordinateOutOfRangeException(Exception):
